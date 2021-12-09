68308721ffb1f6f9c0ce89b4cff76695d7580f22
Evie Viau <evie@uwueviee.live>
2021-12-09 04:28:14 -0500

    Initial working version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,182 @@
+use std::fs::File;
+use std::hash::Hash;
+use std::io::{Cursor, Read, Write};
+use std::path::Path;
+use std::process::exit;
+use ring::digest::{Context, SHA512};
+use serde_derive::Deserialize;
+use rusqlite::{Connection, params};
+use toml::Value::Float;
+use walkdir::{DirEntry, WalkDir};
+use data_encoding::HEXUPPER;
+
+#[derive(Deserialize)]
+struct Config {
+    name: String,
+}
+
+#[derive(Deserialize)]
+struct PackageInfo {
+    name: String,
+    version: String,
+    epoch: i32,
+    description: String,
+    groups: Vec<String>,
+    url: String,
+    license: Vec<String>,
+    depends: Vec<String>,
+    optional_depends: Vec<String>,
+    make_depends: Vec<String>,
+    provides: Vec<String>,
+    conflicts: Vec<String>,
+    replaces: Vec<String>,
+    maintainers: Vec<String>
+}
+
+/// Converts a vec of strings to a flat string separated by ","
+pub fn vec_to_string(vec: &Vec<String>) -> String {
+    let mut temp_string: String = String::new();
+    let mut x: usize = 0;
+    for i in vec {
+        temp_string.push_str(&*i);
+        if !(x == (vec.len() - 1)) {
+            temp_string.push_str(",");
+        }
+        x += 1;
+    }
+    temp_string
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry.file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let current_unix_time = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .expect("Time went backwards?")
+        .as_millis();
+
+    /* UNUSED, but will be in the future
+    if !Path::new("REPOINFO").exists() {
+        println!("This is not a valid repository");
+        exit(1);
+    }
+
+    // Get and load the config file
+    let config_file_contents = std::fs::read_to_string("REPOINFO").unwrap();
+    let config: Config = toml::from_str(&config_file_contents).unwrap();
+
+    println!("=== Generating {} ===", &config.name);
+    */
+
+    println!("=== Generating {} at {} ===", "Database", current_unix_time);
+
+    // Create the initial sqlite database with rust-sqlite
+    println!("=== Creating Database ===");
+    let db = Connection::open("database.db").expect("Failed to create database! Aborting...");
+
+    db.execute(
+        "create table if not exists packages
+            (
+                name             text       not null
+                                            primary key
+                                            unique,
+                version          text       not null,
+                epoch            integer    not null,
+                description      text       not null,
+                groups           text,
+                url              text,
+                license          text,
+                depends          text,
+                build_depends    text,
+                optional_depends text,
+                provides         text,
+                conflicts        text,
+                replaces         text,
+                checksum         text
+            );
+        ",
+        [],
+    ).expect("Failed to insert table into database! Aborting...");
+    println!("=== Database Created! ===");
+
+    println!("=== Populating Database ===");
+    for entry in WalkDir::new("packages/").into_iter()
+        .filter_entry(|e| !is_hidden(e))
+        .filter_map(|e| e.ok()) {
+        if entry.file_name().to_str().unwrap().ends_with("PKGINFO") {
+            // Load the PKGINFO file as JSON and put it into PackageInfo
+            let package_info_file_contents = std::fs::read_to_string(entry.path()).unwrap();
+            let package_info: PackageInfo = serde_json::from_str(&package_info_file_contents).unwrap();
+
+            println!("=> Inserting: {} v{}-{}", &package_info.name, &package_info.version, &package_info.epoch);
+
+            db.execute("
+                INSERT OR REPLACE INTO packages
+                    (
+                        name,
+                        version,
+                        epoch,
+                        description,
+                        groups,
+                        url,
+                        license,
+                        depends,
+                        build_depends,
+                        optional_depends,
+                        provides,
+                        conflicts,
+                        replaces,
+                        checksum
+                    ) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14);
+            ",
+            params![
+                &package_info.name,
+                &package_info.version,
+                &package_info.epoch,
+                &package_info.description,
+                vec_to_string(&package_info.groups),
+                &package_info.url,
+                vec_to_string(&package_info.license),
+                vec_to_string(&package_info.depends),
+                vec_to_string(&package_info.make_depends),
+                vec_to_string(&package_info.optional_depends),
+                vec_to_string(&package_info.provides),
+                vec_to_string(&package_info.conflicts),
+                vec_to_string(&package_info.replaces),
+                ""
+            ]
+            ).expect("Failed to insert package into database! Aborting...");
+        }
+    }
+    println!("=== Database Populated! ===");
+
+    println!("=== Generating Hash ===");
+    let mut database_file = File::open("database.db").expect("Failed to open database.db! Aborting...");
+
+    let mut context = Context::new(&SHA512);
+    let mut buffer = [0; 1024];
+
+    loop {
+        let read = database_file.read(&mut buffer).expect("Failed to read database.db! Aborting...");
+        if read == 0 {
+            break;
+        }
+        context.update(&buffer[..read]);
+    }
+
+    let hash = context.finish();
+
+    println!("=> Hash: {}", HEXUPPER.encode(hash.as_ref()));
+
+    let mut database_hash_file = File::create("database.hash").expect("Failed to create database.hash! Aborting...");
+    database_hash_file.write_all(HEXUPPER.encode(hash.as_ref()).as_bytes()).expect("Failed to write to database.hash! Aborting...");
+    println!("=> Hash File Created!");
+    println!("=== Hash Generated! ===");
+
+    println!("=== Finished! ===")
 }
